313cfe8f89ca4edd34a33e1ff726c2366440eb29
carannantea <79670978+carannantea@users.noreply.github.com>
2021-10-28 20:37:43 -0400

    Added code to add user on signup screen

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
-
     # Points to "Sign Up" screen in design.kv file
     def sign_up(self):
         self.manager.current = "sign_up_screen"
@@ -14,7 +15,20 @@ class RootWidget(ScreenManager):
     pass
 
 class SignUpScreen(Screen):
-    pass
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        # Add new user dictionary to users
+        users[uname] = {'username': uname, 
+                        'password': pword,
+                        'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+
+        # Rewrite json file to add new user
+        with open("users.json", "w") as file:
+            json.dump(users, file)
+
+        print(users)
 
 class MainApp(App):
     def build(self):
