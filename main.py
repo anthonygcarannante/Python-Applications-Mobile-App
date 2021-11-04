74c75e98b79ebe929652569363fd4849defa1670
carannantea <79670978+carannantea@users.noreply.github.com>
2021-11-03 20:58:58 -0400

    Stylized main page

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.animation import Animation
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
 import json, glob, random
 from datetime import datetime
 from pathlib import Path
@@ -68,6 +72,9 @@ class LoginScreenSuccess(Screen):
             self.ids.quote.text = random.choice(quotes)
         else:
             self.ids.quote.text = "Try another feeling."
+
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
 
 class MainApp(App):
     def build(self):
